c5ce2919811271e6466d40b8a583d7ba6d92a77c
Sylvan Mably <sylvan@gmail.com>
2018-12-23 12:57:08 -0500

    Fix card on node 8

--- bin/index.js
+++ bin/index.js
@@ -2,7 +2,7 @@
 
 const verMajor = parseInt(process.versions.node.split(".")[0], 10);
 
-if (verMajor <= 6) {
+if (verMajor <= 8) {
   require("../dist/node6-card");
 } else {
   require("../dist/card");
